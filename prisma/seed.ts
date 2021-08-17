4aecfa8eaa1f3d16fc0aa41038cbd3051b7fc395
Tracer1337 <merlin.moelter@gmail.com>
2021-08-17 22:41:14 +0200

    Add missing dependencies

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,15 @@
 import { PrismaClient } from "@prisma/client"
 import { makeRunnable, run } from "@m.moelter/task-runner"
-import { hash } from "../src/utils"
+import crypto from "crypto"
 
 const client = new PrismaClient()
+
+function hash(input: string) {
+    return crypto
+        .createHash("sha256")
+        .update(input, "utf8")
+        .digest("hex")
+}
 
 async function seedAdmin() {
     const admin = await client.admin.findMany()
