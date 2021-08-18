76bf785159fbce463aada63abb48563c380b8a3f
Tracer1337 <merlin.moelter@gmail.com>
2021-08-18 22:58:40 +0200

    Add JWT guards to product CUD operations

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from "@nestjs/common"
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    ParseIntPipe,
+    Post,
+    Put,
+    UseGuards,
+} from "@nestjs/common"
+import { JWTAuthGuard } from "src/auth/guards/jwt-auth.guard"
 import { CreateProductDTO } from "./dto/create-product.dto"
 import { UpdateProductDTO } from "./dto/update-product.dto"
 import { ProductService } from "./product.service"
@@ -17,11 +28,13 @@ export class ProductController {
         return await this.productService.findById(id)
     }
 
+    @UseGuards(JWTAuthGuard)
     @Post()
     async create(@Body() createProductDTO: CreateProductDTO) {
         await this.productService.create(createProductDTO)
     }
 
+    @UseGuards(JWTAuthGuard)
     @Put(":id")
     async update(
         @Param("id", ParseIntPipe) id: number,
@@ -30,6 +43,7 @@ export class ProductController {
         await this.productService.update(id, updateProductDTO)
     }
 
+    @UseGuards(JWTAuthGuard)
     @Delete(":id")
     async delete(@Param("id", ParseIntPipe) id: number) {
         await this.productService.delete(id)
