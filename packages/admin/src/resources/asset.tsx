67e8b877dd0480bdb7ab2e6b53d27ac981e92fae
Tracer1337 <merlin.moelter@gmail.com>
2021-09-17 13:12:09 +0200

    Add asset preview field

--- packages/admin/src/resources/asset.tsx
+++ packages/admin/src/resources/asset.tsx
@@ -1,8 +1,12 @@
 import {
     List,
     Datagrid,
-    TextField
+    TextField,
+    useRecordContext
 } from "react-admin"
+import { makeURL } from "../api"
+
+const IMAGE_EXTENSIONS = [".jpeg", ".jpg", ".png", ".gif"]
 
 export function AssetList(props: any) {
     return (
@@ -12,7 +16,25 @@ export function AssetList(props: any) {
                 <TextField source="filename"/>
                 <TextField source="type.key" label="Type"/>
                 <TextField source="product.name" label="Product"/>
+                <AssetPreviewField source="filename" label="Preview"/>
             </Datagrid>
         </List>
+    )
+}
+
+function AssetPreviewField(props: any) {
+    const { source } = props
+    const record = useRecordContext(props)
+    const filename: string = record[source]
+    const url = makeURL(`storage/${filename}`)
+
+    if (IMAGE_EXTENSIONS.some((ext) => filename.endsWith(ext))) {
+        return (
+            <img src={url} alt="Preview" width={100}/>
+        )
+    }
+
+    return (
+        <a download href={url}>Download</a>
     )
 }
