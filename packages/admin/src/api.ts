aa82ce446841932a04c320dd23a12ed496b5e44d
Tracer1337 <merlin.moelter@gmail.com>
2021-09-17 17:17:04 +0200

    Use env variables for admin dashboard

--- packages/admin/src/api.ts
+++ packages/admin/src/api.ts
@@ -2,8 +2,6 @@ import { fetchUtils } from "react-admin"
 import queryString from "query-string"
 import { authProvider } from "./authProvider"
 
-const API_BASE_URL = "http://localhost:8080/api/v1"
-
 export function httpClient(url: string, options: fetchUtils.Options = {}) {
     if (!options.headers) {
         options.headers = new Headers({
@@ -22,7 +20,7 @@ export function makeURL(path: string, params?: Record<string, any>) {
     const query = !params ? "" : queryString.stringify(params, {
         arrayFormat: "bracket"
     })
-    return `${API_BASE_URL}/${path}?${query}`
+    return `${process.env.REACT_APP_API_HOST}/api/v1/${path}?${query}`
 }
 
 export function makeAdminURL(...args: Parameters<typeof makeURL>) {
